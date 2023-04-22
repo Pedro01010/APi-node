4837acab8b2470e173577a7fa442184f81528679
pedroA0gusto <114010190+Pedro01010@users.noreply.github.com>
2023-04-21 23:02:21 -0300

    hello express

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+app.use ('/', (req, res) => {
+    res.send('hello, word');
+
+});
+
+app.listen(4000, (err) =>{
+    if(err){
+        return console.log('erro')
+    }
+    console.log('iniciou em http://localhost:4000')
+})
